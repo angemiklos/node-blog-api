e5485d48f02726bf5f905aea0addf20b7f9adcbe
Ange Miklos <angejuba@gmail.com>
2018-04-09 18:20:06 -0700

    added files for module posting of blog object

--- server.js
+++ server.js
@@ -1,55 +1,29 @@
 const express = require('express');
 
-// we're going to use the router
-const router = express.Router();
-
-// include the logger lib and the ability to parse the body
+// include the logger lib 
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-// bring in the BlogPosts object to make it available
-const {BlogPosts} = require('./models');
+// bring in the BlogRouter object to make it available
+const {blogRouter} = require('./blogRouter');
 
-// instantiate the body parser and the express app
-const jsonParser = bodyParser.json();
+// instantiate the express app
 const app = express();
 
 // add logging output
 app.use(morgan('common'));
 
-// Create some blog posts to start with
-const blogpost1 = `
-       The quick red fox jumped over the lazy brown dog.
-`;
-const blogpost2 = `
-       Twas brillig and the slithy toves did gire and gimple in the wabe.
-`;
-const blogpost3 = `
-       One bright day in the middle of the night, two dead boys got up to fight.
-`;
-BlogPosts.create("About foxes", blogpost1, "Ange")
-BlogPosts.create("The Jabberwock", blogpost2, "Dee")
-BlogPosts.create("The Backwards Rhyme", blogpost3, "Sue")
+app.use(express.static('public'));
 
 // React to GET command
-app.get("/blog-posts", (req, res) => {
-    res.json(BlogPosts.get());
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-// React to PUT command
-app.put("/blog-posts/:id", jsonParser, (req, res) => {
-
-});
-
-// React to POST command
-app.post("/blog-posts", jsonParser, (req, res) => {
-
-});
-
-// React to DELETE command
-app.delete("/blog-posts/:id", (req, res) => {
-
-});
+// when requests come into `/blog-posts`,
+// we'll route them to the express
+// router instance we've imported. Remember,
+// this router instance acts as modular, mini-express apps.
+app.use('/blog-posts', blogRouter);
 
 // Listen
 app.listen(process.env.PORT || 8080, () => {
